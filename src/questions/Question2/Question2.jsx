37932663d859a8ad71637e314d5859c6f5388b29
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2023-03-30 23:39:02 -0300

    fix: blocking await in async function

--- src/questions/Question2/Question2.jsx
+++ src/questions/Question2/Question2.jsx
@@ -1,14 +1,16 @@
- async function printValueWithInterval(value, interval) {
-	await new Promise(resolve => setTimeout(resolve, interval * 1000));
-	console.log(value, new Date().toLocaleTimeString());
+function printValueWithInterval(value, interval) {
+  return new Promise(resolve => setTimeout(() => {
+    console.log(value, new Date().toLocaleTimeString());
+    resolve();
+  }, interval * 1000));
 }
 
-export async function printArrayValuesWithDoubleIntervals(arr) {
+export function printArrayValuesWithDoubleIntervals(arr) {
 	console.log('startTime', new Date().toLocaleTimeString());
 	let interval = 1;
 	for (let i = 0; i < arr.length; i++) {
 		try {
-			await printValueWithInterval(arr[i], interval);
+			printValueWithInterval(arr[i], interval);
 			interval *= 2;
 		} catch (error) {
 			console.log(error);

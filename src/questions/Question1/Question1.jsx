1c77145f0f42b3966fcfd476d6d25d3a3e1cebd4
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2023-04-03 14:30:29 -0300

    fix: decrease if statements in question 1

--- src/questions/Question1/Question1.jsx
+++ src/questions/Question1/Question1.jsx
@@ -2,24 +2,16 @@ export function findDuplicates(array) {
   const seenDictionary = new Set();
   const duplicates = new Set();
 
-  array.forEach((item) => {
-		if (item !== null && item !== undefined) {
-			if (typeof item === 'object') {
-				const itemAsString = JSON.stringify(item);
-				if (seenDictionary.has(itemAsString)) {
-					duplicates.add(item);
-				} else {
-					seenDictionary.add(itemAsString);
-				}
-			} else {
-				if (seenDictionary.has(item)) {
-					duplicates.add(item);
-				} else {
-					seenDictionary.add(item);
-				}
-			}
-		}
-  });
+	array.forEach((item) => {
+  if (item === null || item === undefined) return;
+  const itemAsString = (typeof item === 'object') ? JSON.stringify(item) : item;
+  if (seenDictionary.has(itemAsString)) {
+    duplicates.add(item);
+  } else {
+    seenDictionary.add(itemAsString);
+  }
+});
 
 	return Array.from(duplicates);
 }
+
